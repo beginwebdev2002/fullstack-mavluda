03eedc46e36f1ca0c0ffa9c6baa598b55dbbd2c2
Jamoliddin <jamoliddinqodirov18@gmail.com>
2026-05-01 09:40:01 +0500

    chore: initialize Angular project configuration, environment files, and TypeScript path mappings

--- frontend/environments/environment.development.ts
+++ frontend/environments/environment.development.ts
@@ -9,3 +9,4 @@ export interface Environment {
   apiUrl: string;
   telegramBotName: string;
 }
+
---
+++ frontend/environments/environment.tg.ts
@@ -0,0 +1,7 @@
+import { Environment } from "./environment.development";
+
+export const environment: Environment = {
+  production: true,
+  apiUrl: "https://fullstack-mavluda.onrender.com",
+  telegramBotName: "test_bot",
+};
